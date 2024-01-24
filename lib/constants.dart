0d89d1e2003bf8f1ac95afb3e83f684798cc3698
Chaudhary Alekh <98481926+haxor09alert@users.noreply.github.com>
2024-01-24 13:04:37 +0545

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const String google_api_key = "pk.eyJ1IjoicmlzaGFiaDAwNyIsImEiOiJjbGJiMjM5dHEwNDQyM3d1cGZ1bHE0dTEwIn0.Cj0ByZlzzqhu43sH-DHPRA";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
---
+++ lib/order_traking_page.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class OrderTrackingPage extends StatefulWidget {
+  const OrderTrackingPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrderTrackingPage> createState() => OrderTrackingPageState();
+}
+
+class OrderTrackingPageState extends State<OrderTrackingPage> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
+  static const LatLng destination = LatLng(37.33429383, -122.06600055);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Vehicle Track",
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        ),
+      ),
+      body: GoogleMap(initialCameraPosition: CameraPosition(target: sourceLocation, zoom: 14.0),
+      markers:{
+      Marker(
+        markerId: MarkerId("source"),
+        position: sourceLocation,
+          ),
+      Marker(
+        markerId: MarkerId("source"),
+        position: sourceLocation,
+          ),
+        }
+      )
+    );
+  }
+}
